8b9a0e572df892165eadb0c8e19715de68404187
Jarusiri1 <s6404062663118@email.kmtunb.ac.th>
2025-03-15 23:51:58 +0700

    commit

--- app.py
+++ app.py
@@ -125,15 +125,6 @@ if os.path.exists(MODEL_PATH):
 else:
     print("❌ Model not found! Make sure to train and save `model.h5`.")
 
-# ✅ โหลดโมเดลทำนายราคาบ้าน
-MODEL_PATH_2 = "model2.h5"
-model3 = None
-
-if os.path.exists(MODEL_PATH_2):
-    model2 = load_model(MODEL_PATH_2, compile=False)
-    print("✅ House price prediction model loaded successfully!")
-else:
-    print("❌ House price prediction model not found! Make sure to train and save `model3.h5`.")
 
 # ✅ ฟังก์ชันสำหรับพยากรณ์รูปภาพ (หมา หรือ แมว)
 class_names = ["Cat", "Dog"]
@@ -150,28 +141,6 @@ def predict_image(img_path):
     predicted_class = int(prediction[0][0] > 0.5)  # ใช้ threshold 0.5
     return class_names[predicted_class]
 
-# ฟังก์ชันสำหรับพยากรณ์ราคา
-def predict_house_price(features):
-    try:
-        # ตรวจสอบและแปลงข้อมูล
-        features = np.array(features, dtype=np.float32)
-
-        # ตรวจสอบให้แน่ใจว่า shape ของข้อมูลตรงตามที่โมเดลคาดหวัง
-        features = features.reshape(1, -1)
-
-        print("Features shape before prediction:", features.shape)
-
-        # ทำการพยากรณ์
-        prediction = model2.predict(features)
-
-        # ผลลัพธ์การพยากรณ์
-        predicted_price = prediction[0][0]
-        print("Predicted house price:", predicted_price)
-        return predicted_price
-    except Exception as e:
-        print("Error during prediction:", e)
-        return f"Error during prediction: {str(e)}"
-
 
 # ✅ กำหนดโฟลเดอร์สำหรับอัปโหลดรูป
 UPLOAD_FOLDER = "static/uploads"
@@ -182,9 +151,6 @@ os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 def home():
     return render_template('index.html')
 
-@app.route('/about')
-def about():
-    return render_template('about.html')
 
 @app.route('/page1')
 def page1():
@@ -197,27 +163,6 @@ def explaindiabetes():
 @app.route('/explainmnist')
 def explainmnist():
     return render_template('explainmnist.html')
-
-@app.route('/page2', methods=["GET", "POST"])
-def page2():
-    predicted_price = None
-    if request.method == "POST":
-        try:
-            house_size = float(request.form['house_size'])
-            bedrooms = float(request.form['bedrooms'])
-            bathrooms = float(request.form['bathrooms'])
-            living_rooms = float(request.form['living_rooms'])
-            floors = float(request.form['floors'])
-            kitchens = float(request.form['kitchens'])
-            garages = float(request.form['garages'])
-
-            # ส่งข้อมูลไปพยากรณ์
-            features = [house_size, bedrooms, bathrooms, living_rooms, floors, kitchens, garages, 0]  # garden = 0
-            predicted_price = predict_house_price(features)
-        except ValueError:
-            return "Error: Invalid input. Please enter valid numbers for all fields."
-
-    return render_template('page2.html', predicted_price=predicted_price)
 
 
 # ✅ อัปโหลดและพยากรณ์รูปภาพ
